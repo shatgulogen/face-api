d0ec1dc6e9124ae47d56fde5c58c4de09ed53716
ShatgulOgen <tutiyagogo@gmail.com>
2021-03-17 00:23:36 -0400

    Added my project

--- controllers/image.js
+++ controllers/image.js
@@ -1,28 +1,3 @@
-const Clarifai = require('clarifai');
-
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
-    apiKey: '8f38d0dfe64d4565969e932b8e54329c'
-});
-
-const handleAPiCall = (req, res) => {
-    app.models.predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-        'c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
-        .then(data => {
-            res.json(data);
-        })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
-
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -35,6 +10,5 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports{
-    handleImage,
-    handleAPiCall
+    handleImage
 }
--- server.js
+++ server.js
@@ -9,16 +9,25 @@ const register = require('./controllers/register');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
-
 const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+/*const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true 
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
-})
+});*/
 
 
 const app = express();
@@ -28,15 +37,17 @@ app.use(cors());
 
 
 app.get('/', (req, res)=> {
-  res.send('it is working!');
+  res.send(db.users);
 })
 
 app.post('/signin', (res, res) => { signin.handleSignin(req, res, db, bcrypt)})
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port ${process.env.PORT}');
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.listen(3001, ()=> {
+  console.log('app is running on port 3000');
 })
